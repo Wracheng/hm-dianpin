b68d6ec9d0cb3c0f74164e2709911e7f8f3905fb
木城 <cheng.wang@greencloud.com.cn>
2022-11-01 17:38:36 +0800

    全局id

--- src/main/java/com/hmdp/utils/RedisGlobalID.java
+++ src/main/java/com/hmdp/utils/RedisGlobalID.java
@@ -21,15 +21,18 @@ public class RedisGlobalID {
     private final int COUNT_BITS = 32;
     @Resource
     private StringRedisTemplate stringRedisTemplate;
-    public long getId(String prefix){
+    // 获取下一个Id
+    public long nextId(String prefix){
         // 获取时间戳
         LocalDateTime now = LocalDateTime.now();
         long nowStampTime = now.toEpochSecond(ZoneOffset.UTC);
         long stamp = nowStampTime - BEGIN_STAMPTIME;
         // 获取序列号
         String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        // 以增量方式存进redis，常用来做统计
         Long count = stringRedisTemplate.opsForValue().increment("icr:" + prefix + date);
-        // 重点 拼接返回，数值类型的拼接需要移位
+        // 重点 拼接返回，数值类型的拼接需要移位，由于BEGIN_STAMPTIME是long本身占64位但是一般只需要用到后面的32位就行了（而且第一位必须是0，左移32位要作为首位符号位的,31位秒数最多可以表达68年）
+        // 重点 count虽然也是long，也一般只要用到后面的32位，,前32位都是0，由于key是分店铺和每日的，所以2^32次方最多能表示42.9亿，足够用了
         return stamp << COUNT_BITS | count;
     }
 }
--- src/test/java/com/hmdp/Test.java
+++ src/test/java/com/hmdp/Test.java
@@ -11,17 +11,20 @@ import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RedisData;
+import com.hmdp.utils.RedisGlobalID;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import com.baomidou.mybatisplus.extension.service.impl.*;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.*;
 
 /**
  * @author Mr.Wang
@@ -34,6 +37,11 @@ import java.util.Optional;
 public class Test {
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    private RedisGlobalID redisGlobalID;
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(500);
 
     @Resource
     private ServiceImpl<ShopMapper, Shop> s;
@@ -54,5 +62,29 @@ public class Test {
         String redisDataStr = JSONUtil.toJsonStr(redisData);
         stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + 1, redisDataStr);
         System.out.println(shopInfo);
+    }
+    @org.junit.jupiter.api.Test
+    public void globalUniqueRedisId() throws InterruptedException {
+        // 用来让线程执行完毕主线程再执行，300是指有300个线程，因为CountDownLatch的原理是执行完一个线程，线程数会减一，等减到0，主线程就可以执行了，类似async、await
+        CountDownLatch countDownLatch = new CountDownLatch(300);
+        Runnable task = () -> {
+            for(int i = 0; i < 100; i++){
+                long shop1 = redisGlobalID.nextId("shop1");
+                System.out.println(shop1);
+            }
+            // 一个线程执行完让线程数减一
+            countDownLatch.countDown();
+        };
+        long start = System.currentTimeMillis();
+        System.out.println("start=" + start);
+        for(int i = 0; i < 300; i++){
+            executorService.submit(task);
+        }
+        // 线程执行完毕
+        countDownLatch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("end=" + end);
+        System.out.println("消耗的时间=" + (end - start));
+        // 执行完上述操作后，redis中多了一个 key: icr:shop12022:11:01，value： 60000
     }
 }
