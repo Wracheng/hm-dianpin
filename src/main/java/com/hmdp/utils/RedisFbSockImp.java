4db4d330fb7beffd4aa9f1426ef03ff1d73b8a71
木城 <cheng.wang@greencloud.com.cn>
2022-11-04 17:42:25 +0800

    增加注释

--- src/main/java/com/hmdp/utils/RedisFbSockImp.java
+++ src/main/java/com/hmdp/utils/RedisFbSockImp.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
  */
 @Component
 public class RedisFbSockImp implements RedisFbSock {
-    // 键名，包含业务名，用户id（如果想要再精确一点再加个优惠券id）
+    // 键名，包含业务名，用户id（如果想要再精确一点再加个优惠券id），做到一人一锁
     private String name;
     private final String PREFIX = "lock:";
     private final String PREFIX_UUID = UUID.randomUUID().toString(true) + "-";
@@ -32,7 +32,7 @@ public class RedisFbSockImp implements RedisFbSock {
 
     @Override
     public boolean tryGetLock(long timeout) {
-        // 1、判断锁存不存在
+        // 1、判断锁存不存在 （这一段是原子操作）
         Boolean isGetLock = stringRedisTemplate.opsForValue().setIfAbsent(PREFIX + name, PREFIX_UUID + Thread.currentThread().getId() + "", timeout, TimeUnit.SECONDS);
         // 避免isGetLock为null产生拆箱空指针异常
         return Boolean.TRUE.equals(isGetLock);
